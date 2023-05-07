f9806c9430c832343ae1184fb02918473a656339
ThiagoBertachini <thiago.bertachini@hotmail.com>
2023-05-07 00:30:33 -0300

    including flyway to migrating db and jsonProperty to serialize json

---
+++ api-rest-aws/src/main/resources/db/migration/V2__Insert_Into_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` VALUES 
+	(1, 'Mock Name 1', 'Mock Last Name 1', 'Mock Address 1', 'male'),
+	(2, 'Mock Name 2', 'Mock Last Name 2', 'Mock Address 2', 'female'),
+	(3, 'Mock Name 3', 'Mock Last Name 3', 'Mock Address 3', 'male');
